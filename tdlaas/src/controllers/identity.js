90c372a1f9272b287b16b203dbd36688da4e4c48
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2025-04-24 22:39:48 +0700

    fix: use nonce from redis

--- tdlaas/src/controllers/identity.js
+++ tdlaas/src/controllers/identity.js
@@ -1,27 +1,17 @@
 const {
-  Credential,
-  Duration,
   EdDSAJwsVerifier,
   FailFast,
   IotaIdentityClient,
-  JwkMemStore,
-  JwsSignatureOptions,
   JwsVerificationOptions,
   Jwt,
   JwtCredentialValidationOptions,
   JwtCredentialValidator,
-  JwtPresentationOptions,
   JwtPresentationValidationOptions,
   JwtPresentationValidator,
-  KeyIdMemStore,
-  Presentation,
   Resolver,
-  Storage,
-  SubjectHolderRelationship,
-  Timestamp
+  SubjectHolderRelationship
 } = require('@iota/identity-wasm/node')
 const { Client } = require('@iota/sdk-wasm/node')
-const { createDid } = require('../utils/did')
 const crypto = require('crypto')
 const redisClient = require('../config/redis')
 
@@ -40,88 +30,19 @@ exports.initConnection = async (req, res) => {
     })
 }
 
-// HANDLE DATA DYNAMICALLY
-exports.createVP = async (req, res) => {
-  const client = new Client({
-    primaryNode: process.env.API_ENDPOINT,
-    localPow: true
-  })
-
-  const issuerSecretManager = { mnemonic: process.env.TEST_MNEMONIC_1 }
-  const issuerStorage = new Storage(new JwkMemStore(), new KeyIdMemStore())
-  const { document: issuerDocument, fragment: issuerFragment } =
-    await createDid(client, issuerSecretManager, issuerStorage)
-
-  const aliceSecretManager = { mnemonic: process.env.TEST_MNEMONIC_2 }
-  const aliceStorage = new Storage(new JwkMemStore(), new KeyIdMemStore())
-  const { document: aliceDocument, fragment: aliceFragment } = await createDid(
-    client,
-    aliceSecretManager,
-    aliceStorage
-  )
-
-  const subject = {
-    id: aliceDocument.id(),
-    name: 'Alice',
-    degreeName: 'Bachelor of Science and Arts',
-    degreeType: 'BachelorDegree',
-    GPA: '4.0'
-  }
-
-  const unsignedVc = new Credential({
-    id: 'https://example.edu/credentials/3732',
-    type: 'UniversityDegreeCredential',
-    issuer: issuerDocument.id(),
-    credentialSubject: subject
-  })
-
-  const credentialJwt = await issuerDocument.createCredentialJwt(
-    issuerStorage,
-    issuerFragment,
-    unsignedVc,
-    new JwsSignatureOptions()
-  )
-
-  const response = new JwtCredentialValidator(new EdDSAJwsVerifier()).validate(
-    credentialJwt,
-    issuerDocument,
-    new JwtCredentialValidationOptions(),
-    FailFast.FirstError
-  )
-  console.log('Credential Validation:', response.intoCredential())
-
-  const nonce = crypto.randomBytes(16).toString('hex')
-  const expires = Timestamp.nowUTC().checkedAdd(Duration.minutes(30))
-
-  nonceStore.set(nonce, { did: aliceDocument.id().toString(), expires })
-
-  const unsignedVp = new Presentation({
-    holder: aliceDocument.id(),
-    verifiableCredential: [credentialJwt]
-  })
-
-  const presentationJwt = await aliceDocument.createPresentationJwt(
-    aliceStorage,
-    aliceFragment,
-    unsignedVp,
-    new JwsSignatureOptions({ nonce }),
-    new JwtPresentationOptions({ expirationDate: expires })
-  )
-
-  res.status(200).json({
-    presentationJwt: presentationJwt.toString(),
-    nonce,
-    vp: unsignedVp.toJSON()
-  })
-}
-
-// HANDLE DATA DYNAMICALLY
 exports.verifyVP = async (req, res) => {
   try {
-    const { presentationJwt, nonce } = req.body
+    const { presentationJwt, uniqueId } = req.body
 
-    if (!presentationJwt || !nonce) {
-      throw new Error('Missing required parameters: presentationJwt and nonce')
+    if (!presentationJwt || !uniqueId) {
+      throw new Error(
+        'Missing required parameters: presentationJwt and uniqueId'
+      )
+    }
+
+    const nonce = await redisClient.get(`id:${uniqueId}`)
+    if (!nonce) {
+      throw new Error('Invalid or expired nonce')
     }
 
     const client = new Client({
@@ -182,6 +103,9 @@ exports.verifyVP = async (req, res) => {
       )
       credentialValidations.push(validation.intoCredential())
     }
+
+    // Delete the nonce after successful validation
+    await redisClient.del(`id:${uniqueId}`)
 
     res.status(200).json({
       message: 'VP validated successfully!',
--- tdlaas/src/routes/identity.js
+++ tdlaas/src/routes/identity.js
@@ -1,9 +1,8 @@
 const router = require('express').Router()
 
-const { initConnection, createVP, verifyVP } = require('../controllers/identity')
+const { initConnection, verifyVP } = require('../controllers/identity')
 
 router.post('/init/:id', initConnection)
-router.post('/create', createVP)
 router.post('/verify', verifyVP)
 
 module.exports = router
