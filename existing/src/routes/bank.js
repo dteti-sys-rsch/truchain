54820f2aa9edc167ede698115925a58ceb31a307
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2025-09-23 21:18:06 +0700

    feat: checkpoint for main

--- existing/src/routes/bank.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-const { createVerifiedBank } = require('../controllers/bank')
-
-router.post('/', createVerifiedBank)
-
-module.exports = router
--- existing/src/routes/transaction.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-const { createTransaction, queryTransactions } = require('../controllers/transaction')
-
-router.post('/', createTransaction)
-router.get('/', queryTransactions)
-
-module.exports = router
